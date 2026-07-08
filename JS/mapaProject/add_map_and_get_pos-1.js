3a63a704ac61e259b9dba2a6d2d2ddde122bd415
Arturo <arturoperezygarcia@gmail.com>
2026-07-07 20:40:48 -0600

    Remove windsock marker

--- JS/mapaProject/add_map_and_get_pos-1.js
+++ JS/mapaProject/add_map_and_get_pos-1.js
@@ -113,10 +113,3 @@ updateDistanceFromCurrent(currentLocation);
 console.log("Global lat: ", lat," Global lng: ", lng);
 });
 
-//se genera el marker de windsock
-var svgCono = document.createElement('div');
-svgCono.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="100px" height="100px" baseProfile="basic"><polygon fill="#a8a8a8" points="43,27.309 43,34.238 37,37.702 17,30.774 17,23.845 14,20.381 14,44.053 12,45.207 10,44.053 10,19.804 8,18.65 8,14.03 10,12.88 10,7.102 12,5.95 14,7.102 14,12.88 16,14.03 16,15.762 23.004,15.76 27,13.453"/><polygon fill="gray" points="12,16.34 12,20.959 16,18.649 16,14.03"/><polygon fill="#ccc" points="12,16.34 12,20.959 8,18.649 8,14.03"/><polygon fill="#737373" points="12,8.257 12,15.185 14,14.03 14,7.102"/><polygon fill="#ccc" points="12,8.257 12,15.185 10,14.03 10,7.102"/><polygon fill="#737373" points="12,20.959 12,45.207 14,44.053 14,19.804"/><polygon fill="#bababa" points="12,20.959 12,45.207 10,44.053 10,19.804"/><polygon fill="#a8a8a8" points="13,16.917 15,15.762 23,15.762 21,16.917"/><polygon fill="#ccc" points="21,16.917 13,16.917 13,19.227 17,23.845 17,21.536 15,19.227 17,19.226"/><polygon fill="#d94a4a" points="37,30.774 17,19.226 27,13.453 43,27.309"/><polygon fill="#ff5757" points="37,30.774 37,37.702 17,30.774 17,19.226"/><polygon fill="#b33d3d" points="43,27.309 43,34.238 37,37.702 37,30.774"/><polygon fill="#ff5757" points="38,31.351 42,29.041 42,33.66"/><polygon fill="#e64e4e" points="38,31.351 38,35.97 42,33.66"/><polygon fill="#ccc" points="21,21.536 21,32.158 25,33.543 25,23.845"/><polygon fill="#a8a8a8" points="25,23.845 33.399,18.996 30.2,16.224 21,21.536"/><polygon fill="#ccc" points="29,26.155 29,34.93 33,36.315 33,28.464"/><polygon fill="#a8a8a8" points="33,28.464 39.799,24.539 36.599,21.768 29,26.155"/></svg>';
-
-var svgMarker = new mapboxgl.Marker(svgCono)
-.setLngLat([-92.26, 14.9])
-.addTo(mapboxMap);
